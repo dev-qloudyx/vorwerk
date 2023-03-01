cdaae32b68054cecf6a84e1e1dde85d8ed823e20
anaalopes <ana.lopes@qloudyx.pt>
2023-03-01 10:36:30 +0000

    Estrutura Lojas

--- apps/locais/urls.py
+++ apps/locais/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, reverse_lazy
+from .views import LocalListView
 
 app_name = "locais"
 
 urlpatterns = [
-    
+    path('lojas/', LocalListView.as_view(), name='lojas'),
 ]
--- apps/locais/views.py
+++ apps/locais/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Local
 
 # Create your views here.
+class LocalListView(ListView):
+    model = Local
+    template_name = 'locais/local_list.html'
+    context_object_name = 'locais'
+    ordering = ['nome']
+
+
+    
